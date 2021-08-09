56f2d1a9771c2e609c9a48301db9f40384e09053
Mehmet <mehmetkusken@gmail.com>
2021-08-09 16:57:13 -0600

    commit tree af7500626e406265df107eae36b97beafaf2cdd2

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 
+const testVar = {};
+function testFunc() {
+    return "hi";
+  }
+  const record = [
+    { year: "2018", result: "L" },
+    { year: "2017", result: "W" },
+    { year: "2016", result: "N/A" }
+  
+  ];
+  function superbowlWin(arrOfObjects) {
+    let win = arrOfObjects.find(obj => {
+        return obj.result === "W";
+      });
+      return win ? win.year : undefined;
+}
